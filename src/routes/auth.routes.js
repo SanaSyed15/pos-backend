f28f2a19856384169c06bf0b280af4fd21102d61
SanaSyed <sanasyedrihan15@gmail.com>
2026-04-30 16:58:14 +0530

    fix deployment config 2

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,9 +1,6 @@
 import express from "express";
-import {
-  login,
-  superAdminForgotPassword,
-  resetPassword
-} from "../controllers/auth.controller.js";
+import { login, superAdminForgotPassword, resetPassword } from "../controllers/auth.controller.js";
+
 const router = express.Router();
 
 router.post("/login", login);
